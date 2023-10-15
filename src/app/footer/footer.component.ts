f8207092bfda60375cefd30ad1490c820ae08f19
Victor Morelli <victor.morelli.007@gmail.com>
2023-10-15 19:29:26 -0300

    footer year is now calculated, and not hard-coded anymore

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  getCurrentYear(): number {
+    return new Date().getFullYear();
+  }
 }
