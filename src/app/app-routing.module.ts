0ff52bb73960d5bfc73f96f747853fe1754ebc5e
Victor Morelli <victor.morelli.007@gmail.com>
2023-10-29 21:13:42 -0300

    removed useless components, approached a route query logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
+import { MovieListComponent } from './movie-list/movie-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PopularComponent } from './popular/popular.component';
-import { MoviesComponent } from './movies/movies.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { WatchlistComponent } from './watchlist/watchlist.component';
-
 const routes: Routes = [
-  { path: 'popular', component: PopularComponent },
-  { path: 'movies', component: MoviesComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: 'watchlist', component: WatchlistComponent },
+  { path: 'movies/popular', component: MovieListComponent },
+  { path: 'movies/favorites', component: MovieListComponent },
   { path: '', redirectTo: '/popular', pathMatch: 'full' } // redirect to popular as default
 ];
 
