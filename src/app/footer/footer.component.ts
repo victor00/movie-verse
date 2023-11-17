e53499ce94b614afed8503d2668f4c76c9b6df4f
Victor Morelli <victor.morelli.007@gmail.com>
2023-11-17 17:54:16 -0300

    fixed footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  isTargetRoute: boolean = false;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.checkRoute();
+    });
+  }
+
+  private checkRoute() {
+    let route = this.activatedRoute.snapshot;
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+
+    const isCreateMovie = route.routeConfig?.path === 'create/movie';
+
+    const currentPath = route.params['category'];
+
+    console.log(route.params['category'])
+    this.isTargetRoute = isCreateMovie ||
+      currentPath === 'favorites' ||
+      currentPath === 'watchlist';
+  }
+
   getCurrentYear(): number {
     return new Date().getFullYear();
   }
