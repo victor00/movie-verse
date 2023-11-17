1ca626cc19baf984f3895c6d737ee530ded5d7bd
Victor Morelli <victor.morelli.007@gmail.com>
2023-11-17 16:30:33 -0300

    make sidebar/navbar more realiable for responsiveness

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const M: any;
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  private Materialize: any;
 
+  ngOnInit() {
+    this.Materialize = M;
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const elems = document.querySelectorAll('.sidenav');
+      const instances = this.Materialize.Sidenav.init(elems, {});
+    });
+  }
 }
